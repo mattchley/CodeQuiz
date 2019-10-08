28d756ee27e479751af60caada19c21ee5a6da6c
mattchley <mattchley@gmail.com>
2019-10-08 13:38:08 -0700

    renderedDivs

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Universal Elements
+var startPageEl = document.getElementById('startpage')
+var startBtnEl = document.getElementById('startbtn')
+var scoreDisplayEl = document.getElementById('score')
+var timerDisplayEl = document.getElementById('timer')
+var quizContentEl = document.getElementById('quizcontent')
+var answerA = document.getElementById('a')
+var answerB = document.getElementById('b')
+var answerC = document.getElementById('c')
+var answerD = document.getElementById('d')
+var userNamerInputEl = document.getElementById('username')
+var questionAr = 
+// the start page must render something like this
+<div id="container">
+        <h1>Coding Quiz Challenge</h1>
+        <p>Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers
+            will penalize your score time by 10 seconds.</p>
+        <button id="startBtn">Start Quiz</button>
+    </div>
+
+
 // Timer script
 // taken from activity 9 for a 5 second count down. [Needs to be a 75 second count down: can be set by putting the timeLeft = 75?, must execute end function when timer is zero]
 function prepareRead() {
@@ -20,8 +41,26 @@ function prepareRead() {
 
 // Question generator
 
+// Answer Check
+
 // Event listener for start button
-
+startBtn.addEventListener('click', function(begin)){
+var lastQuestionIndex = questions.length - 1;
+var runningQuestionIndex = 0;
+var q = question[runningQuestionIndex];
+function (begin){
+question.innerHTML = "<p>" + q.question + "</p>";
+answerA.innerHTML = q.answerA;
+answerB.innerHTML = q.answerB;
+answerC.innerHTML = q.answerC;
+answerD.innerHTML = q.answerD;
+}
+}
 // Event listener for any question to start the next question
+nextQuestionBtn.addEventListener('click', function(next)){
 
+}
 // input space for user name that local stores the name and score
+userNamerInputEl.addEventListener('click', function(save)){
+
+}
